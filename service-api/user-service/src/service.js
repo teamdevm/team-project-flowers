c68061ad9d7734dfec7970c23f088a14af16ff5f
humbleAcolyte <bayandin.eugene@gmail.com>
2022-06-01 00:41:07 +0500

    Implemented service file

--- service-api/user-service/src/service.js
+++ service-api/user-service/src/service.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const conf = require('./configs/service')
+const user = require('./routes/userRoute');
+const {sequelize} = require('./models/models');
 
+const app = express();
+
+app.use('/', user);
+
+app.listen(conf.port, (err) => {
+    if (err) {
+        return console.log('Something bad happened:\n\t', err);
+    }
+
+    sequelize.sync().catch(error => console.log('Something bad happened with db:\n\t', error));
+
+    console.log(`Server is listening on port ${conf.port}`);
+})
