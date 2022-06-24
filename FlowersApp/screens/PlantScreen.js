381a5de51ea5a78499cc3d8100faf3ebfbeb8d15
vaechh <john.ermakov27@gmail.com>
2022-06-24 19:08:33 +0500

    Added plant delete

--- FlowersApp/screens/PlantScreen.js
+++ FlowersApp/screens/PlantScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Button, StyleSheet, Text, View} from "react-native";
 import TrashBtn from '../components/TrashBtn';
 import Flower from '../Assets/niceflower.svg';
+import HeaderEditBtn from "../components/HeaderEditBtn";
 
 export default class PlantScreen extends React.Component{
     constructor(props) {
@@ -33,6 +34,37 @@ export default class PlantScreen extends React.Component{
 
     async componentDidMount() {
         await this.getPlantInfo();
+        this.props.navigation.setOptions(
+            {
+                headerRight: () => (
+                    <HeaderEditBtn
+                        textStyle={[styles.text.header,styles.text.headerBtn]}
+                        //onPress={() => this.props.navigation.navigate('AddPlant')}
+                    />
+                ),
+                headerTitle:'Инфо'
+            }
+        )
+    }
+
+    async deletePlant(){
+        try {
+            const response = await fetch(`http://46.146.230.198:3000/plant/${this.state.plant.id}`,{
+                method: 'DELETE',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.ok)
+            {
+                this.props.navigation.goBack();
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
     }
 
     render(){
@@ -46,15 +78,15 @@ export default class PlantScreen extends React.Component{
                         <Flower width={'100%'} height={'100%'}/>
                     </View>
 
-                    <Text style={styles.text}>
+                    <Text style={styles.infoText}>
                         {plant.name}
                     </Text>
 
                     <View style={{alignItems:'center'}}>
-                        <Text style={styles.text}>
+                        <Text style={styles.infoText}>
                             Дата последнего полива:
                         </Text>
-                        <Text style={styles.text}>
+                        <Text style={styles.infoText}>
                             {plant.lastWater}
                         </Text>
                     </View>
@@ -66,7 +98,9 @@ export default class PlantScreen extends React.Component{
                 </View>
 
                 <View style={styles.trashContainer}>
-                    <TrashBtn width={54} height={54}/>
+                    <TrashBtn width={54} height={54} onOk={async ()=> {
+                        await this.deletePlant()
+                    }}/>
                 </View>
 
             </View>
@@ -80,9 +114,10 @@ const styles = StyleSheet.create({
         flexDirection:'column',
         height:'100%'
     },
-    text:{
+    infoText:{
         color:'#575757',
         fontSize:26,
+        textAlign:'center'
     },
     flower:{
         flex:0.7,
@@ -106,5 +141,17 @@ const styles = StyleSheet.create({
         justifyContent:'flex-end',
         alignItems:'flex-end',
         margin:20
+    },
+    text:
+    {
+        header:{
+            color:'#575757',
+        },
+        headerBtn:{
+            fontSize:40
+        },
+        headerTitle:{
+            fontSize:30
+        }
     }
 });
