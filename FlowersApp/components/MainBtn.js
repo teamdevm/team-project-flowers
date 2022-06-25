274ec26b8b2c3e272759410583ead0fac398c1d3
vaechh <john.ermakov27@gmail.com>
2022-06-25 21:09:43 +0500

    Cosmetic update

--- FlowersApp/components/MainBtn.js
+++ FlowersApp/components/MainBtn.js
@@ -16,6 +16,7 @@ const styles = StyleSheet.create({
         flexDirection:'column',
         justifyContent:'center',
         alignItems:'center',
+        alignSelf:'center',
         backgroundColor:'#339933',
         height:50,
         width:130,
--- FlowersApp/components/PlantCard.js
+++ FlowersApp/components/PlantCard.js
@@ -8,7 +8,7 @@ class PlantCard extends Component{
         return (
             <TouchableOpacity style={styles.container}
             onPress={()=>this.props.navigation.navigate(screenName,{id:plant.id})}>
-                <Text style={{fontSize:30, color:'#575757'}}>
+                <Text style={{fontSize:23, color:'#575757',textAlign:'center'}}>
                     {plant.name}
                 </Text>
             </TouchableOpacity>
@@ -20,6 +20,7 @@ export default withNavigation(PlantCard);
 const styles = StyleSheet.create({
     container:{
         backgroundColor:'#ffffff',
+        borderColor:'#BDBDBD',
         width:'90%',
         borderRadius:15,
         borderWidth:2,
@@ -27,7 +28,7 @@ const styles = StyleSheet.create({
         alignSelf:'center',
         justifyContent:'center',
         marginTop:30,
-        height:60,
+        minHeight:60,
         //maxWidth:1000,
         alignItems:'center',
         //marginLeft:'5%'
